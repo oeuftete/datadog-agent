fe413fe31937467a179d57944680627a98f8f45e
Derek Brown <derek.brown@datadoghq.com>
2023-07-26 11:17:59 -0700

    review feedback

--- pkg/collector/corechecks/system/wincrashdetect/probe/crashparse.go
+++ pkg/collector/corechecks/system/wincrashdetect/probe/crashparse.go
@@ -26,7 +26,16 @@ type logCallbackContext struct {
 	unfinished     string
 }
 
-const maxscan = int(200)
+// maximum number of stack trace lines we'll look through, looking for non-"NT!" lines
+const maxLinesToScan = int(200)
+
+const (
+	bugcheckCodePrefix     = "Bugcheck code"
+	debugSessionTimePrefix = "Debug session time"
+	retAddrPrefix          = "RetAddr"
+	unableToLoadPrefix     = "Unable to"
+	ntBangPrefix           = "nt!"
+)
 
 func min(a, b int) int {
 	if a < b {
@@ -34,11 +43,6 @@ func min(a, b int) int {
 	}
 	return b
 }
-
-const (
-	bugcheckCodePrefix     = "Bugcheck code"
-	debugSessionTimePrefix = "Debug session time"
-)
 
 //export logLineCallback
 func logLineCallback(voidctx C.PVOID, str C.PCSTR) {
@@ -65,7 +69,7 @@ func logLineCallback(voidctx C.PVOID, str C.PCSTR) {
 				ctx.loglines = append(ctx.loglines, l)
 				return
 			}
-			if strings.HasPrefix(l, "RetAddr") {
+			if strings.HasPrefix(l, retAddrPrefix) {
 				ctx.hasSeenRetAddr = true
 				start = idx
 			}
@@ -96,9 +100,35 @@ func parseCrashDump(wcs *WinCrashStatus) {
 		return
 	}
 
-	end := min(len(ctx.loglines)-1, maxscan)
+	// set a maximum of how many lines we'll scan looking for NT!.  The loglinecallback
+	// above should strip off all the lines until the first `RetAddr` line.  So the number of
+	// lines we need to see "should" be on the order of 5.  Set an (arbitrary) max that if
+	// we don't find anything, we're not going to.
+
+	/* expect the lines to look something like:
+	Arguments ffffffff`c0000005 fffff806`f7e010e6 ffffb481`789326a8 ffffb481`78931ef0
+
+	RetAddr           : Args to Child                                                           : Call Site
+	fffff800`457f4db0 : 00000000`0000007e ffffffff`c0000005 fffff806`f7e010e6 ffffb481`789326a8 : nt!KeBugCheckEx
+	fffff800`457cb7bf : 00000000`00000004 00000000`00000000 00007fff`ffff0000 ffffc582`1b4e3800 : nt!memset+0x5530
+	fffff800`457e602d : ffffb481`78933000 ffffb481`789318c0 00000000`00000000 00000000`00000050 : nt!_C_specific_handler+0x9
+	f
+	fffff800`457742a1 : ffffb481`78933000 00000000`00000000 ffffb481`7892d000 00000000`00000000 : nt!_chkstk+0x5d
+	fffff800`457730c4 : ffffb481`789326a8 ffffb481`789323f0 ffffb481`789326a8 ffffb481`78932570 : nt!KeQuerySystemTimePrecis
+	e+0x27d1
+	fffff800`457ee482 : 00003c74`00000000 fffff800`458a1d00 00000000`00000000 fffff800`45d940c4 : nt!KeQuerySystemTimePrecis
+	e+0x15f4
+	fffff800`457eafc0 : 00000000`00000000 fffff800`45a97fe0 ffff8301`2c077220 ffffc582`1bb72c30 : nt!setjmpex+0x7622
+	fffff806`f7e010e6 : 00000000`00000001 00000000`00000000 ffffb481`76e2e000 fffff800`456e6511 : nt!setjmpex+0x4160
+	*** ERROR: Module load completed but symbols could not be loaded for ddapmcrash.sys
+	fffff806`f7e07020 : ffffc582`1bb72c30 ffffc582`19f18000 ffffc582`1bb72c30 ffff3ac8`f399d666 : ddapmcrash+0x10e6
+
+	So, even though the loglinecallback will strip out, we might see the "symbols could not be loaded line".  We could
+	also see additional RetAddr headers.*/
+
+	end := min(len(ctx.loglines)-1, maxLinesToScan)
 	for _, line := range ctx.loglines[:end] {
-		// skip lines that start with RetAddr, that's just the header
+
 		if strings.HasPrefix(line, debugSessionTimePrefix) {
 			parts := strings.SplitN(line, ":", 2)
 			if len(parts) == 2 {
@@ -112,10 +142,14 @@ func parseCrashDump(wcs *WinCrashStatus) {
 			wcs.BugCheck = codeAsString
 			continue
 		}
-		if strings.HasPrefix(line, "RetAddr") {
+		// skip lines that start with RetAddr, that's just the header
+		if strings.HasPrefix(line, retAddrPrefix) {
 			continue
 		}
-		if strings.HasPrefix(line, "Unable to") { // "Unable to load image, which is ok
+		// as shown above, there might be a stray "symbols could not be loaded line".  This would then
+		// cause the split on ":" below  to not work, and then things would get worse from there.  so
+		// just skip this line because it's expected.
+		if strings.HasPrefix(line, unableToLoadPrefix) { // "Unable to load image, which is ok
 			continue
 		}
 		parts := strings.Split(line, ":")
@@ -123,7 +157,7 @@ func parseCrashDump(wcs *WinCrashStatus) {
 			continue
 		}
 		callsite := strings.TrimSpace(parts[2])
-		if strings.HasPrefix(callsite, "nt!") {
+		if strings.HasPrefix(callsite, ntBangPrefix) {
 			// we're still in ntoskernel, keep looking
 			continue
 		}
